3c59a803ac38994ee37e713511d5faf5589e04cc
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-09-28 08:43:23 -0400

    Fix issues with jsonconverter.

--- jsonconverter.go
+++ jsonconverter.go
@@ -10,31 +10,11 @@ import (
 	"os"
 )
 
-// FieldsReader reads fields from tab separated file
-type FieldsReader struct {
-	*csv.Reader
-	fields []int
-}
-
-func (r *FieldsReader) Read() (record []string, err error) {
-	rec, err := r.Reader.Read()
-	if err != nil {
-		return nil, err
-	}
-
-	record = make([]string, len(r.fields))
-	for i, f := range r.fields {
-		record[i] = rec[f]
-	}
-
-	return record, nil
-}
-
 func main() {
 	rows := make([]map[string]string, 0)
-	var columns = []string{"name", "email", "phone"}
+	var columns = []string{"language", "frequency"}
 	csvPtr := flag.String("csv-file", "", "CSV File to parse. (Required)")
-	tsvPtr := flag.String("tsv-file", "", "CSV File to parse. (Required)")
+	tsvPtr := flag.String("tsv-file", "", "TSV File to parse. (Required)")
 	flag.Parse()
 
 	if *csvPtr == "" && *tsvPtr == "" {
@@ -67,7 +47,8 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println(data)
+		// print the reformatted struct as JSON
+		fmt.Printf("%s\n", data)
 	} else {
 		f, err := os.Open(*tsvPtr)
 		if err != nil {
@@ -75,14 +56,26 @@ func main() {
 		}
 		defer f.Close()
 		reader := csv.NewReader(bufio.NewReader(f))
-		reader.Comma = '\t' // Use tab-delimited instead of comma <---- here!
-		tsvFile, err := reader.ReadAll()
+		reader.Comma = '\t' // Use tab-delimited instead of comma
+		for {
+			record, err := reader.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Println(err)
+			}
+			row := make(map[string]string)
+			for i, n := range columns {
+				row[n] = record[i]
+			}
+			rows = append(rows, row)
+		}
+		data, err := json.MarshalIndent(&rows, "", "  ")
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(1)
 		}
-		for field, each := range tsvFile {
-			fmt.Println(field, each)
-		}
+		// print the reformatted struct as JSON
+		fmt.Printf("%s\n", data)
 	}
 }

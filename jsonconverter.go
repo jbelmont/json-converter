26c48cc8646835a424ab452a41e8e70904941a86
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-09-28 09:00:19 -0400

    Add function for jsonconverter instead of duplication.

--- jsonconverter.go
+++ jsonconverter.go
@@ -10,9 +10,38 @@ import (
 	"os"
 )
 
-func main() {
+func convertToJSON(f *os.File, isTSVFile bool) {
 	rows := make([]map[string]string, 0)
 	var columns = []string{"language", "frequency"}
+	// Create a new reader.
+	csvReader := csv.NewReader(bufio.NewReader(f))
+	csvReader.TrimLeadingSpace = true
+	if isTSVFile {
+		csvReader.Comma = '\t' // Use tab-delimited instead of comma
+	}
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		row := make(map[string]string)
+		for i, n := range columns {
+			row[n] = record[i]
+		}
+		rows = append(rows, row)
+	}
+	data, err := json.MarshalIndent(&rows, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// print the reformatted struct as JSON
+	fmt.Printf("%s\n", data)
+}
+
+func main() {
 	csvPtr := flag.String("csv-file", "", "CSV File to parse. (Required)")
 	tsvPtr := flag.String("tsv-file", "", "TSV File to parse. (Required)")
 	flag.Parse()
@@ -26,56 +55,13 @@ func main() {
 			fmt.Println(err)
 		}
 		defer f.Close()
-		// Create a new reader.
-		csvReader := csv.NewReader(bufio.NewReader(f))
-		csvReader.TrimLeadingSpace = true
-		for {
-			record, err := csvReader.Read()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				fmt.Println(err)
-			}
-			row := make(map[string]string)
-			for i, n := range columns {
-				row[n] = record[i]
-			}
-			rows = append(rows, row)
-		}
-		data, err := json.MarshalIndent(&rows, "", "  ")
-		if err != nil {
-			fmt.Println(err)
-		}
-		// print the reformatted struct as JSON
-		fmt.Printf("%s\n", data)
+		convertToJSON(f, false)
 	} else {
 		f, err := os.Open(*tsvPtr)
 		if err != nil {
 			fmt.Println(err)
 		}
 		defer f.Close()
-		reader := csv.NewReader(bufio.NewReader(f))
-		reader.Comma = '\t' // Use tab-delimited instead of comma
-		for {
-			record, err := reader.Read()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				fmt.Println(err)
-			}
-			row := make(map[string]string)
-			for i, n := range columns {
-				row[n] = record[i]
-			}
-			rows = append(rows, row)
-		}
-		data, err := json.MarshalIndent(&rows, "", "  ")
-		if err != nil {
-			fmt.Println(err)
-		}
-		// print the reformatted struct as JSON
-		fmt.Printf("%s\n", data)
+		convertToJSON(f, true)
 	}
 }
